377692b798dc254dc2e7dc6fd2603af2524bf8f7
Flairseed <rameshpvb@gmail.com>
2024-11-30 05:09:28 +0800

    feat: create user register function

--- backend/users_posts/functions/database-functions.js
+++ backend/users_posts/functions/database-functions.js
@@ -14,5 +14,31 @@ async function connectToDatabase() {
       port: process.env.PORT,
       database: process.env.DATABASE,
     });
+  };
+}
+
+async function userRegister(req) {
+  const valid = validateUser(req);
+
+  if (!valid) {
+    return response(400, displayErrors(validateUser.errors));
+  }
+
+  try {
+    const sql = "INSERT INTO user(name, pasword) VALUES(?, ?)";
+    const queryValues = [
+      req.name,
+      req.pasword,
+    ];
+    
+    await connection.query(sql, queryValues);
+    return response(200, {
+      message: "Successfully registered.",
+    });
+  } catch (err) {
+    console.log(err);
+    return response(500, {
+      message: "Internal server error.",
+    });
   }
 }
