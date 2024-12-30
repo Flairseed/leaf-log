0781e54a92e0f0c6dfdbe9c5f3d065cbe2d8f555
Flairseed <rameshpvb@gmail.com>
2024-12-31 01:08:00 +0530

    build: import room library and ksp plugin

--- LeafLog/app/build.gradle.kts
+++ LeafLog/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -50,6 +51,9 @@ android {
 }
 
 dependencies {
+    // Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
