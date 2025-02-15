0f1910bf5af722d7a1c186aece7711a8fac0c9b0
Flairseed <rameshpvb@gmail.com>
2025-02-16 03:40:47 +0800

    update: add new field to log

--- LeafLog/app/src/main/java/com/example/leaflog/feature_log/data/model/Log.kt
+++ LeafLog/app/src/main/java/com/example/leaflog/feature_log/data/model/Log.kt
@@ -2,10 +2,21 @@ package com.example.leaflog.feature_log.data.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.example.leaflog.feature_journal.data.model.Journal
 import java.util.Date
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Journal::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("journal_id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Log(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "journal_id") val journalId: Int,
@@ -18,4 +29,5 @@ data class Log(
     val temperature: Int?,
     val picture: String,
     val created: Date,
+    val postId: Int? = null
 )
